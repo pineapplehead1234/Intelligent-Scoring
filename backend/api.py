c0cc67778d6c1166f6e6aa2cf1c26f769479f4bb
BBBiriccc <2787982907@qq.com>
2025-03-14 00:21:13 +0800

    部分注释和calculate_similarity部分代码的修改

--- backend/api.py
+++ backend/api.py
@@ -6,26 +6,36 @@ from transformers import pipeline
 from sentence_transformers import SentenceTransformer
 import tempfile
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import torch
+import numpy as np
 
-
-os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
-
+os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"   # os.environ 是一个字典，用于访问和修改环境变量。
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"              # 可以使用 os.environ["KEY"] 来获取环境变量的值，
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"             # 也可以使用 os.environ["KEY"] = "VALUE" 来设置环境变量的值。
+                                                      # OneDNN 加速 TensorFlow 在英特尔 CPU 上的计算的功能被禁用
 # 创建线程池
 pool = ThreadPoolExecutor(max_workers=5)
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # Cross-Origin Resource Sharing，简称 CORS，是一种机制，
+# 它使用额外的 HTTP 头来告诉浏览器 让运行在一个 origin (domain) 上的 Web 应用被准许访问来自不同源服务器上的指定资源。
+# 当一个跨源 HTTP 请求，例如从一个域名的网页上发起的请求，
+# 这个请求的响应将会包含一些 CORS 相关的头信息，
+# 浏览器会根据这些头信息来决定是否允许这个请求。
 
 UPLOAD_FOLDER = tempfile.gettempdir()
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+# 检查是否有可用的 CUDA 设备，如有则返回 0 并且使用 GPU，否则使用 CPU
+device = 0 if torch.cuda.is_available() else -1
 
 transcriber = pipeline(task="automatic-speech-recognition", model="openai/whisper-tiny.en")
 similarity_model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def speech2text(speech_file):
+    # 调用 transcriber 函数处理语音文件，并返回包含时间戳信息的字典
     text_dict = transcriber(speech_file, return_timestamps=True)
+    # 从返回的字典中提取文本内容并返回
     return text_dict["text"]
 
 def calculate_similarity(transcripts, standard_answer):
@@ -33,8 +43,16 @@ def calculate_similarity(transcripts, standard_answer):
     embeddings = similarity_model.encode(sentences)
     standard_embedding = embeddings[-1]
     scores = []
-    for i, embedding in enumerate(embeddings[:-1]):
-        score = similarity_model.similarity([embedding], [standard_embedding])[0][0].item()
+    # 将 embeddings 转换为 numpy 数组
+    # 将 standard_embedding 转换为 numpy 数组
+    # 使用 torch.cosine_similarity 计算余弦相似度
+    # 由于 torch.tensor() 只能接受单个 numpy.ndarray 作为输入，不能接受列表，
+    # 故 numpy.ndarray 列表创建 torch.tensor 速度极慢 ，
+    # 先使用 numpy.array() 把列表转换为单个 numpy.ndarray ，再转换为 torch.tensor
+    embeddings_np = np.array(embeddings)
+    standard_embedding_np = np.array(standard_embedding)
+    for i, embedding in enumerate(embeddings_np[:-1]):
+        score = torch.cosine_similarity(torch.tensor([embedding]), torch.tensor([standard_embedding_np]))[0].item()
         rounded_score = round(score, 5)
         scores.append(rounded_score)
     return scores
